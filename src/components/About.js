63b995c7ac6573af69baabfd1ef9a69fd1db97d4
Minaj <minajinamdar9@gmail.com>
2024-02-26 21:14:26 +0530

    about us page fetching github user api

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,53 @@
-import Team from "./Team"
+import React from "react";
 
-const About = () =>{
-    return(
-        <div className="about-container">
-        <div className="about">
-            <h1>Welcome to food apps </h1>
-            <h2>We are delevering food and happines for last 10 F years</h2>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim repellat vitae deleniti omnis iusto reprehenderit aut
-                id asperiores inventore, quaerat nostrum praesentium hic maiores commodi obcaecati tenetur, mollitia voluptatibus
-                cumque aperiam assumenda quis nobis excepturi?
-            </p>
-            <h3>So trust us and just us a try</h3>
-            <h2>Let's Introduce</h2>
-            <Team name="Minaj Inamdar" designation="CEO"/>
-        </div>
-    </div>
-    )
+
+class About extends React.Component{
+    constructor(){
+        super()
+
+        this.state = {
+            userData : {
+                name: "...",
+                location: "---"
+            }
+            
+        }
+        console.log("constructor")
+    }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/gitminaj");
+
+        const json = await data.json();
+        
+        this.setState({userData : json})
+
+    }
+
+    
+    render(){
+        const {name,location} = this.state.userData;
+        return(
+            <div className="about-container">
+                <div className="about">
+                    <h1>Name: {name}</h1>
+                    <h1>location: {location}</h1>
+                    <h1>designation: CEO<span><sub className="small-font">future</sub></span></h1>
+                </div>
+            </div>
+        )
+    }
+
 }
+
+
+// const About = () =>{
+//     return(
+//     <div className="about-container">
+
+//     </div>
+//     )
+// }
 
 
 export default About;
